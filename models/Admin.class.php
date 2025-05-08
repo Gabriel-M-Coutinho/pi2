27424eb960e7cd1dc7ee38c35c310e169026cf86
Gabriel Coutinho <122622768+Gabriel-M-Coutinho@users.noreply.github.com>
2025-05-08 11:49:33 -0300

    first commit

---
+++ models/Admin.class.php
@@ -0,0 +1,22 @@
+<?php
+final class Admin extends User
+{
+    public function __construct(
+        private string $type = ""
+    )
+    {
+        parent:: __construct($name, $email, $password);
+    }
+
+    public function getType()
+    {
+        return $this->type;
+    }
+
+
+    public function setType($type)
+    {
+        $this->type = $type;
+    }
+}
+?>
---
+++ models/User.Class.php
@@ -0,0 +1,42 @@
+<?php
+abstract class User{
+    public function __construct(
+        private string $name = "",
+        private string $email = "",
+        private string $password = ""
+    ) {}
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
+
+}
+
+?>
